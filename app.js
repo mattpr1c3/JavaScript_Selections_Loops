c1b0feb7cbd2db4908a6aec16e67ff9f180018bc
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-14 13:33:14 -0700

    did exercises 1 and 2

--- app.js
+++ app.js
@@ -3,5 +3,27 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 1);
+  console.log(i);
+}
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FIZZBUZZ");
+  } else if (i % 3 == 0) {
+    console.log("FIZZ");
+  } else if (i % 5 == 0) {
+    console.log("BUZZ");
+  }
+}
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+let i = 1;
+
+while (i <= 100)
